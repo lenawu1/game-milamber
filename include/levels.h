1c8f8556b2b2bdf8d0b49de04bff5c0a37c1e7b0
Emily Choe <ecchoe@caltech.edu>
2021-05-20 00:20:01 -0700

    d

---
+++ include/levels.h
@@ -0,0 +1,9 @@
+#ifndef __LEVELS_H__
+#define __LEVELS_H__
+
+#include <stdbool.h>
+#include "body.h"
+
+
+
+#endif // ifndef __LEVELS_H__
---
+++ library/levels.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "levels.h"
