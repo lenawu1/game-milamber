9af79b89dd22a526453040172f737283f7b84696
bshan <bshan@caltech.edu>
2021-05-28 21:03:42 +0800

    point scores working

--- demo/level_1.c
+++ demo/level_1.c
@@ -20,6 +20,13 @@
 #include "terrain.h"
 #include "level_handlers.h"
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+
+
 // const double SCREEN_SIZE_Y = 750;
 // const double SCREEN_SIZE_X = 750;
 const vector_t SCREEN_SIZE = {.x = 2000, .y = 1000};
@@ -54,10 +61,21 @@ void handler(char key, key_event_type_t type, double held_time, scene_t *scene) 
     }
 }
 
+// void update_text(scene_t *scene) {
+//     TTF_Font *font = TTF_OpenFont("resources/OpenSans-Regular.ttf", 12);
+//     SDL_Color textColor = {0, 0, 0, 255}; // Black
+//     SDL_Color textBackgroundColor = {255, 255, 255, 255}; // White
+//     char score_str[10];
+//     SDL_Surface *textSurface = TTF_RenderText_Shaded(font, itoa(scene_get_points(scene), score_str, 10),
+//                                                      textColor, textBackgroundColor);
+//     SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, textSurface);
+// }
 
 int main(int argc, char *argv[]) {
     sdl_init(VEC_ZERO, SCREEN_SIZE);
+    
     scene_t *scene = scene_init();
+
     list_t *ball_elements = create_golf_ball(BALL_SIZE, rgb_color_init(205, 99, 75), BALL_MASS, INIT_POS);
     body_t *player = list_get(ball_elements, 0);
 
@@ -65,16 +83,16 @@ int main(int argc, char *argv[]) {
         scene_add_body(scene, list_get(ball_elements, i));
     }
     generate_level1(scene, player);
-    sdl_on_key(handler,scene);
-    center_display("starting");
+    sdl_on_key(handler, scene);
+    // center_display("starting");
     double clock = 0.0;
     
     while (!sdl_is_done(scene) && scene_get_level(scene) == 1 && scene_get_state(scene) == 0){
         double dt = time_since_last_tick();
         clock += dt;
-
-        //double curr_points = (double)scene_get_points(scene);
-        //point_display(curr_points);
+        // char score_str[10];
+        // sprintf(score_str, "%zu", scene_get_points(scene));
+        // point_display(score_str);
         do_gravity(player, GRAV_VAL, dt);
         scene_tick(scene, dt);
         sdl_render_scene(scene);
