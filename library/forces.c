22326783f7ab44354c3cb6838f0a6fb9d0ab987a
emin <emin@caltech.edu>
2021-06-02 14:44:22 -0700

    fixed sound error and lags.

--- library/forces.c
+++ library/forces.c
@@ -259,10 +259,10 @@ void collision_force_creator(collision_aux_t *auxil, list_t *bodies) {
             return;
         }
         else if (!auxil->collided) {
-            /*char *teleportsound = "../resources/teleport.wav";
+            char *teleportsound = "../resources/teleport.wav";
             if(get_type(body2) == PORTAL) {
                 sdl_load_sound(teleportsound);
-            }*/
+            }
             handler(body1, body2, info.axis, coaux);
         }
     }
